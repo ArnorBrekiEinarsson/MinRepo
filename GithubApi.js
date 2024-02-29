745566931a40f7eb373bd384102872d6b9330846
Arnor Breki <breki25@gmail.com>
2024-02-29 20:08:19 +0100

    added headers to jquery for json during fetch

--- GithubApi.js
+++ GithubApi.js
@@ -1,5 +1,9 @@
 $.ajax ({
-    url: 'https://github.com/repos/ArnorBrekiEinarsson/MinRepo',
+    url: 'https://github.com/ArnorBrekiEinarsson/MinRepo',
+    headers: {
+       "Accept": "application/json",
+       //"Authorization": "Bearer <token>"  
+    },
     success: function(response) {
         console.log(response)
     }
